b60062d1977c17d6161d4a6a363c939efcd852ab
mfahmifadh <mfahmifadh@gmail.com>
2022-09-09 00:13:41 +0700

    laporan super user

--- resources/views/v_super_user/apk_oldat/daftar_laporan.blade.php
+++ resources/views/v_super_user/apk_oldat/daftar_laporan.blade.php
@@ -6,12 +6,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Laporan Pengelolaan Olah Data (OLDAT)</h1>
+                <h1 class="m-0">Daftar Barang</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#"> Dashboard OLDAT</a></li>
-                    <li class="breadcrumb-item active">Dashboard OLDAT</li>
+                    <li class="breadcrumb-item"><a href="{{ url('super-user/oldat/dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Barang</li>
                 </ol>
             </div>
         </div>
@@ -19,22 +19,77 @@
 </div>
 
 <section class="content">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                @if ($message = Session::get('success'))
-                <div class="alert alert-success">
-                    <p style="margin: auto;">{{ $message }}</p>
+    <div class="container-fluid">
+        <div class="col-md-12 form-group">
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p style="color:white;margin: auto;">{{ $message }}</p>
+            </div>
+            @elseif ($message = Session::get('failed'))
+            <div class="alert alert-danger">
+                <p style="color:white;margin: auto;">{{ $message }}</p>
+            </div>
+            @endif
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <div class="card-tools">
+                    <a href="{{ url('super-user/tim-kerja/download/data') }}" class="btn btn-primary" title="Download File" onclick="return confirm('Download data Kategori Barang ?')">
+                        <i class="fas fa-file-download"></i>
+                    </a>
                 </div>
-                @endif
-                @if ($message = Session::get('failed'))
-                <div class="alert alert-danger">
-                    <p style="margin: auto;">{{ $message }}</p>
-                </div>
-                @endif
+            </div>
+            <div class="card-body">
+                <table id="table-barang" class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kode Barang</th>
+                            <th>NUP</th>
+                            <th>Nama Barang</th>
+                            <th>Merk</th>
+                            <th>Jumlah</th>
+                            <th>Satuan</th>
+                            <th>Tahun Perolehan</th>
+                            <th>Kondisi</th>
+                            <th>Pengguna</th>
+                        </tr>
+                    </thead>
+                    <?php $no = 1; ?>
+                    <tbody>
+                        @foreach($barang as $row)
+                        <tr>
+                            <td>{{ $no++ }}</td>
+                            <td>{{ $row->kode_barang }}</td>
+                            <td>{{ $row->nup_barang }}</td>
+                            <td>{{ $row->kategori_barang }}</td>
+                            <td>{{ $row->spesifikasi_barang }}</td>
+                            <td>{{ $row->jumlah_barang }}</td>
+                            <td>{{ $row->satuan_barang }}</td>
+                            <td>{{ $row->tahun_perolehan }}</td>
+                            <td>{{ $row->kondisi_barang }}</td>
+                            <td>{{ $row->nama_pegawai }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </section>
+
+@section('js')
+<script>
+    $(function() {
+        $("#table-barang").DataTable({
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "buttons": ["pdf"],
+            "lengthMenu": [[10, 25, 50, "Semua", -1], [10, 25, 50, "Semua"]]
+        }).buttons().container().appendTo('#table-barang_wrapper .col-md-6:eq(0)');
+    });
+</script>
+@endsection
 
 @endsection
