f4ea2fb9cae342a00b258e5e3ecf1ef6244ba099
mfahmifadhs <mfahmifadh2@gmail.com>
2022-12-28 11:16:42 +0700

    model atk

--- app/Models/ATK/Atk.php
+++ app/Models/ATK/Atk.php
@@ -4,7 +4,7 @@ namespace App\Models\atk;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\ATK\KategoriATK;
+use App\Models\ATK\KategoriAtk;
 
 class Atk extends Model
 {
