a7c14481df908e4d214f2f0051502e5909d6a653
mfahmifadhs <mfahmifadh2@gmail.com>
2023-01-02 18:31:25 +0700

    usulan ukt detail model

--- app/Models/UKT/UsulanUkt.php
+++ app/Models/UKT/UsulanUkt.php
@@ -4,6 +4,7 @@ namespace App\Models\Ukt;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\UKT\UsulanUktDetail;
 
 class UsulanUkt extends Model
 {
