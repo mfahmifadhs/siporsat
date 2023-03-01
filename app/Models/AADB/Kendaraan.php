ee827ee46da43bc9cfe92c255c18a023ee2f9dfa
mfahmifadhs <mfahmifadh2@gmail.com>
2023-03-01 13:47:35 +0700

    soft delete aadb

--- app/Models/AADB/Kendaraan.php
+++ app/Models/AADB/Kendaraan.php
@@ -4,10 +4,13 @@ namespace App\Models\AADB;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kendaraan extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $table        = "aadb_tbl_kendaraan";
     protected $primaryKey   = "id_kendaraan";
     public $timestamps      = false;
